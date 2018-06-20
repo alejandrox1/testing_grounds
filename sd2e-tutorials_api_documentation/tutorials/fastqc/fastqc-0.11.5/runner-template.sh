ba29cd712915a7fd9b1f8563a542887f0456df6b
alejandrox1 <alarcj137@gmail.com>
2018-06-20 13:57:12 -0500

    sd2e-tutorials_api_documentation/tutorials/fastqc/fastqc-0.11.5/runner-template.sh added run template script for fastqc container
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- sd2e-tutorials_api_documentation/tutorials/fastqc/fastqc-0.11.5/runner-template.sh
+++ sd2e-tutorials_api_documentation/tutorials/fastqc/fastqc-0.11.5/runner-template.sh
@@ -1,8 +1,9 @@
-CONTAINER_IMAGE="index.docker.io/sd2e/hello-container:0.1.0"
+CONTAINER_IMAGE="index.docker.io/alejandrox1/fastqc:0.11.5"
 
 . _util/container_exec.sh
 
-COMMAND='bash'
-PARAMS='/opt/scripts/hello.sh WORLD'
+
+COMMAND="fastqc"
+PARAMS="${fastqc}" # $fastqc will be defined later by agave
 
 container_exec ${CONTAINER_IMAGE} ${COMMAND} ${PARAMS}
