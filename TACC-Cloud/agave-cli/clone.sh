3875ce3959e45ac9e638ae0545cbbe0a01b5845d
alejandrox1 <alarcj137@gmail.com>
2018-06-29 16:25:50 -0500

    TACC-Cloud/agave-cli/clone.sh fixed bad cd
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- TACC-Cloud/agave-cli/clone.sh
+++ TACC-Cloud/agave-cli/clone.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/alejandrox1/agave-cli
 
-cd cli && git remote add upstream https://github.com/TACC-Cloud/agave-cli
+cd agave-cli && git remote add upstream https://github.com/TACC-Cloud/agave-cli
