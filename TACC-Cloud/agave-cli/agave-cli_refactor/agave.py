3370bf25ce4f411fe8bed6fa15872536939386a6
alejandrox1 <alarcj137@gmail.com>
2018-07-02 14:10:30 -0500

    TACC-Cloud/agave-cli/agave-cli_refactor/agave.py cleaned up script
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- TACC-Cloud/agave-cli/agave-cli_refactor/agave.py
+++ TACC-Cloud/agave-cli/agave-cli_refactor/agave.py
@@ -9,34 +9,11 @@ import sys
 import tenants
 
 
-def greet(arguments):
-    """Greet
-
-    greet takes arguments parsed from argparse and creates a greeting.
-
-    PARAMETERS
-    ----------
-    arguments: object (argparse.Namespace)
-    """
-    output = "{0}, {1}!".format(arguments.greeting, arguments.name)
-    if arguments.caps:
-        output = output.upper()
-    print(output)
-
-
 # Parser and subparsers definition.
 parser = argparse.ArgumentParser()
 parser.add_argument("--version", action="version", version="0.0.1")
 subparsers = parser.add_subparsers()
 
-# Example command line option.
-hello_parser = subparsers.add_parser("hello")
-hello_parser.add_argument("name", help="name of person to greet")
-hello_parser.add_argument(
-    "-g", "--greeting", default="Hello", help="greeting to use")
-hello_parser.add_argument(
-    "-c", "--caps", action="store_true", help="uppercase output")
-hello_parser.set_defaults(func=greet)
 
 # Tenant command line option.
 tenant_parser = subparsers.add_parser("tenant")
@@ -48,6 +25,9 @@ tenant_parser.add_argument(
     default="https://api.tacc.utexas.edu/tenants",
     help="URL of Agave central service")
 tenant_parser.set_defaults(func=tenants.tenants_list)
+
+
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
