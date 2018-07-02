0ce67e86887c175f7ecdec71680f5b85f3dd4674
alejandrox1 <alarcj137@gmail.com>
2018-07-02 14:11:21 -0500

    TACC-Cloud/agave-cli/agave-cli_refactor/test_agave.py cleaned up tests
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- TACC-Cloud/agave-cli/agave-cli_refactor/test_agave.py
+++ TACC-Cloud/agave-cli/agave-cli_refactor/test_agave.py
@@ -4,21 +4,6 @@ import agave
 import requests
 
 
-def test_hello_parser(capfd):
-    args = agave.parser.parse_args(["hello", "--greeting", "hi", "john"])
-    args.func(args)
-    out, err = capfd.readouterr()
-    assert out == "hi, john!\n"
-    assert err == ""
-
-
-def test_hello_parser_caps(capfd):
-    args = agave.parser.parse_args(["hello", "-c", "-g", "Hola", "john"])
-    args.func(args)
-    out, err = capfd.readouterr()
-    assert out == "HOLA, JOHN!\n"
-    assert err == ""
-
 
 def test_tenant_ls(capfd):
     args = agave.parser.parse_args(["tenant", "ls"])
