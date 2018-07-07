d2eb7e3174dc53556132b719307903b0431241c3
alejandrox1 <alarcj137@gmail.com>
2018-07-07 13:11:30 -0500

    cyverse/cyverse-sdk/clone_cyverse-sdk.sh delete cyverse-sdk repo if it already exists
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- cyverse/cyverse-sdk/clone_cyverse-sdk.sh
+++ cyverse/cyverse-sdk/clone_cyverse-sdk.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf cyverse-sdk
+
 git clone https://github.com/alejandrox1/cyverse-sdk
 
-( cd cyverse-sdk && git remote add https://github.com/cyverse/cyverse-sdk )
+( cd cyverse-sdk && git remote add upsream https://github.com/cyverse/cyverse-sdk )
