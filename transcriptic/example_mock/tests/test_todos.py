ef20201b6b3f88a73b3bbcdc0127a3263a1aad36
alejandrox1 <alarcj137@gmail.com>
2018-07-13 14:25:19 -0500

    transcriptic/example_mock example mock
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

---
+++ transcriptic/example_mock/tests/test_todos.py
@@ -0,0 +1,23 @@
+from unittest.mock import Mock, patch
+from project.services import get_todos
+
+
+@patch('project.services.requests.get')
+def test_getting_todos_when_response_is_ok(mock_get):
+    todos = [{
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }]
+
+    # Configure the mock to return a response with an OK status code. Also, the mock should have
+    # a `json()` method that returns a list of todos.
+    mock_get.return_value = Mock(ok=True)
+    mock_get.return_value.json.return_value = todos
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert response.json() == todos
